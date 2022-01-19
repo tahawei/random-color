f0706bf0b1418eadd65534735ecccb346019e4af
tahawei <amer.zp582@gmail.com>
2022-01-19 19:40:04 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+let hexArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+let colorParts = [];
+for (let i = 0; i < 6; i++) {
+  colorParts.push(hexArray[Math.floor(Math.random() * hexArray.length)]);
+}
+let colorHtmTag = document.createElement("h1");
+let colorHexText = document.createTextNode(`#${colorParts.join("")}`);
+colorHtmTag.append(colorHexText);
+document.body.prepend(colorHtmTag);
+
+colorHtmTag.classList.add("color-hex");
+
+let copyBtn = document.querySelector(".copy");
+copyBtn.style.backgroundColor = `#${colorParts.join("")}`;
+
+document.body.style.backgroundColor = `#${colorParts.join("")}`;
+
+// programming copy btn
+
+let alertText = document.querySelector(".alert");
+
+copyBtn.addEventListener("click", () => {
+  let inputElement = document.createElement("input");
+  inputElement.type = "text";
+  inputElement.value = `#${colorParts.join("")}`;
+  document.body.appendChild(inputElement);
+  inputElement.select();
+  document.execCommand("copy");
+  document.body.removeChild(inputElement);
+  alertText.classList.add("db");
+  alertText.style.transition = "0.5s ease";
+
+  setTimeout(() => {
+    alertText.classList.remove("db");
+  }, 2000);
+});
